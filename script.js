ce97be979353c29dc187f5e21c64c0f32a28bef7
ManonLef <81025586+ManonLef@users.noreply.github.com>
2021-04-25 22:58:41 +0200

    added playRound function

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-//Your game is going to play against the computer, so begin with a function called computerPlay 
-//that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. 
-//Tip: use the console to make sure this is returning the expected output before moving to the next step!
+
+
 
 function computerPlay() {
     let compChoice = ["rock", "paper", "scissors"]; // note to self: mind the brackets [] for math
-    let compRandom = (compChoice[Math.floor(Math.random() * compChoice.length)]);
-    console.log (compRandom);
+    let computerSelection = (compChoice[Math.floor(Math.random() * compChoice.length)]);
+    console.log("computer chose " + computerSelection);
+    return(computerSelection);
+    
 }
 
-computerPlay();
+let computerSelection = computerPlay();
+    let playerSelection = "rock";
+
+function playRound() {
+    if (playerSelection === computerSelection) { 
+        return("it's a tie");   
+    } else if (playerSelection !== computerSelection) {
+    return("bumbum");
+}
+}
+
+
+
+console.log(playRound(playerSelection, computerSelection));
 
 //Write a function that plays a single round of Rock Paper Scissors. 
 //The function should take two parameters - the playerSelection and computerSelection - 
 //and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-//Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+//Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation)
