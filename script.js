610ae45981a769cd3bfd391d6d624d8f86be56f3
ManonLef <81025586+ManonLef@users.noreply.github.com>
2021-04-25 23:54:11 +0200

    inserted all rps options

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-
-
-
 function computerPlay() {
     let compChoice = ["rock", "paper", "scissors"]; // note to self: mind the brackets [] for math
     let computerSelection = (compChoice[Math.floor(Math.random() * compChoice.length)]);
-    console.log("computer chose " + computerSelection);
-    return(computerSelection);
-    
+    console.log(computerSelection);
+    return(computerSelection); 
 }
 
 let computerSelection = computerPlay();
-    let playerSelection = "rock";
+let playerSelection = window.prompt("What will you throw at R2-D2?");
 
-function playRound() {
-    if (playerSelection === computerSelection) { 
-        return("it's a tie");   
-    } else if (playerSelection !== computerSelection) {
-    return("bumbum");
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) { 
+        return("your objects collided mid-air, it's a tie");   
+    } else if (playerSelection.toLowerCase === "rock" && computerSelection === "paper") {
+        return("you lose, R2-D2 had some paper to devour your rock");
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        return("those scissors stood no chance against your mighty rock, you win!");
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        return("you win, This paper must have come from Uranus, it entirely blocked R2-D2's rock.");
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+        return("R2-D2 whips out his Swiss army knife and cuts your paper to pieces. Obvious loss.");
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+        return("you lose, those blades got pretty roughed up by R2D2's rock");
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        return("you win, you cut R2's paper into world's most tiny violin and threw it back.");
+    }
 }
-}
-
-
 
 console.log(playRound(playerSelection, computerSelection));
 
